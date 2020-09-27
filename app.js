411411dab0cf4ef515264b38818c8d2d4a0c81c6
akashali20 <71965233+akashali20@users.noreply.github.com>
2020-09-27 15:21:58 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function show_papsifst()
+{
+document.getElementById('papsi-img').src="img/p1.png";
+document.getElementsByClassName('main')[0].style.backgroundColor="red";
+}
+
+function show_papsisec()
+{
+document.getElementById('papsi-img').src="img/p2.png";
+document.getElementsByClassName('main')[0].style.backgroundColor="red";
+}
+
+function show_papsithrd()
+{
+document.getElementById('papsi-img').src="img/p3.png";
+document.getElementsByClassName('main')[0].style.backgroundColor="green";
+}
